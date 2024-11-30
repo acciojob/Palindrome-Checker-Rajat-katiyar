95ad6b4b71b0e08522a4efba07ecf864e9c8607c
Rajat Katiyar <123207263+Rajat-katiyar@users.noreply.github.com>
2024-11-30 10:50:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // complete the given function
 
 function palindrome(str){
+	const normalizedStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();  
+    
+    // Check if the string is the same forwards and backwards  
+    const reversedStr = normalizedStr.split('').reverse().join('');  
+    
+    return normalizedStr === reversedStr;  
 
 }
 module.exports = palindrome
